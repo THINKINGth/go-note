dab577486ec506a00af7290e8f164e53c7dc63f0
The Starry <44313681+THINKINGth@users.noreply.github.com>
2019-06-01 00:42:10 +0800

    Update main.go

--- go-struct/movie/main/main.go
+++ go-struct/movie/main/main.go
@@ -9,13 +9,6 @@ import (
 )
 
 func main(){
-	mv := movie.New(author.Author{*Person.New("1", "女", "13", "5", "1993")},
-	"神圣的战争", Feature.Feature{})
-	var pt Prints.Printer
-	pt = mv
-	pt.PrintInf()
-	bir := *Person.Person{}.New("2", "女", 14, 6, 1996)
-	mvs := movie.New(author.Author{bir}, "红军最强大", Feature.Feature{})
-	mvs.PrintInf()
+	
 }
 
